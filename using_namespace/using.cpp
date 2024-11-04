694f547c5284bb0ddd5403e66d564e01690437c1
unknown <dileepsathya2205@gmail.com>
2024-11-04 19:08:58 +0530

    d4

---
+++ using_namespace/using.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main(){
+    cout<<"Enter two numbers"<<endl;
+    int v1,v2;
+    cin>>v1>>v2;
+    cout<<"THe sume is "<<v1+v2<<endl;
+}
