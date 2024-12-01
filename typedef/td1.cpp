3a7e9b4948286aba585df8ab0aa10945ba9d0e88
unknown <dileepsathya2205@gmail.com>
2024-12-01 15:25:27 +0530

    second commit

---
+++ typedef/td1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+typedef std::string txt;
+int main(){
+    txt name="DS";
+    std::cout<<name<<std::endl;
+    return 0;
+}
---
+++ typedef/td2_using.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using txt=std::string;
+int main(){
+    txt name="DS";
+    std::cout<<name<<std::endl;
+    return 0;
+}
